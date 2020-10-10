654224053760bc3b37d0b59e8f089e5977eae2ae
sftmaruf <you@example.com>
2020-10-11 00:53:57 +0600

    database connected

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require('express');
+var cors = require('cors');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('working');
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f1z8e.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect(err => {
+
+
+
+    app.get('/volunteerWorks', (req, res) => {
+        const workDB = client.db(process.env.DB_NAME).collection(process.env.DB_COLLECTIONONE);
+        const volunteersDB = client.db(process.env.DB_NAME).collection(process.env.DB_COLLECTIONTWO);
+        
+        workDB.find({})
+            .toArray((err, works) => {
+                res.send(works);
+            })
+    })
 });
 
 app.listen(process.env.PORT || 5000);
