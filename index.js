451b7ec6cd9fb912d2a6e921b9de5a4947501a75
sftmaruf <you@example.com>
2020-10-11 10:29:20 +0600

    firebase admin fixed

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 1
 admin.initializeApp({
     credential: admin.credential.cert({
         "type": process.env.TYPE,
-        "project_id": process.env.PROJECT_ID.replace(/\\n/g, '\n'),
+        "project_id": process.env.PROJECT_ID,
         "private_key_id": process.env.PRIVATE_KEY_ID,
-        "private_key": process.env.PRIVATE_KEY,
+        "private_key": JSON.parse(process.env.PRIVATE_KEY).replace(/\\n/g, '\n'),
         "client_email": process.env.CLIENT_EMAIL,
         "client_id": process.env.CLIENT_ID,
         "auth_uri": process.env.AUTH_URI,
